14b26d96435526c0c43466acc353d5dcdc88ef80
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-05 17:36:52 +0600

    basic type

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name='Shah Jalal' />
+      <Greet name='Shah Jalal' messageCount={10} isLoggedIn={true} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,17 @@
 type GreetProps = {
     name: string
+    messageCount: number
+    isLoggedIn: boolean
 }
 
 export const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>Alhamdulillah! {props.name} is learning typescript.</h2>
+            {
+                props.isLoggedIn ? <div><h2>Alhamdulillah! {props.name} is learning typescript.</h2>
+                    <h3>You have {props.messageCount} unread message.</h3></div> : "Welcome Guest"
+            }
+
         </div>
     )
 
